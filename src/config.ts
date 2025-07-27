2e26af9c1ec31014a23bb9fe37385e71bdf69058
Maxime Moreillon <m.moreillon@gmail.com>
2025-07-28 07:31:39 +0900

    stopped env validation

--- src/config.ts
+++ src/config.ts
@@ -1,15 +1,6 @@
-import { z } from "zod";
-
-const envSchema = z.object({
-  OIDC_SESSION_SECRET: z.string(),
-  OIDC_AUTHORITY: z.string(),
-  OIDC_CLIENT_ID: z.string(),
-  OIDC_COOKIES_INSECURE: z.string().optional(),
-});
-
 export const {
-  OIDC_SESSION_SECRET,
-  OIDC_AUTHORITY,
-  OIDC_CLIENT_ID,
+  OIDC_SESSION_SECRET = "areallylongsecretthatyoushouldreplace",
+  OIDC_AUTHORITY = "",
+  OIDC_CLIENT_ID = "",
   OIDC_COOKIES_INSECURE,
-} = envSchema.parse(process.env);
+} = process.env;
